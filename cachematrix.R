b8180d9954df3e7f26c2e083917e501e482e1ef6
kuanb <kuanbutts@yahoo.com>
2014-07-26 14:07:28 -0400

    added my code over the example place holders

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Short comment describing this function
+## makeCacheMatrix is a class basically, it establishes var x in the class as a matrix, and once the class action
+## of set is called, it takes in that x matrix and sets it or caches/holds it
+##
+## getInverse and setInverse deal with feeding in the inverse solution and then returning it when it is requested
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv  <- NULL
+  set  <- function(y){
+    x <<- y
+    i <<- NULL 
+  }
+  get  <- function() { return(x) }
+  setInverse  <- function(inverse) { inv  <<- inverse }
+  getInverse  <- function() { return(inv) }
+  list(set= set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+  
 }
 
-
-## Write a short comment describing this function
+## cacheSolve is similar to the example cacheMean - it just checks if the inverse already exists in the makeCacheMat class
+## then will return if it already exists, so long as trueTest is true (makes sure the mat is still the same)
+## else it does the same as it would otherwise which is run solve() and get inverse, then setInverse in the matCache class
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv  <- x$getinverse()
+  if (!is.null(inv)){
+    trueTest <- all(x2 == x)
+    if (trueTest == TRUE) {
+      message("getting cached matrix data")
+      return(inv)
+    }
+    else {
+      data  <- x$get()
+      i  <- solve(data, ...)
+      x$setInverse(inv)
+      return(inv)      
+    }
+  }
+  data  <- x$get()
+  i  <- solve(data, ...)
+  x$setInverse(inv)
+  return(inv)
 }
